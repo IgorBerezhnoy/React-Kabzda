e6f1aa759a528c9720d1da3f283a5e4c0da401e0
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-05-23 19:05:37 +0800

    finished lesson number 1 one more time

--- src/App.tsx
+++ src/App.tsx
@@ -2,37 +2,37 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
     return (
         <div className="App">
-            This is react page
-            <Rating />
-            <Accordion />
+            This is component
+            <Rating/>
+            <Accordion/>
         </div>
     );
 }
 
 function Rating() {
-    return (
-        <div>
-            <div>start</div>
-            <div>start</div>
-            <div>start</div>
-            <div>start</div>
-            <div>start</div>
-        </div>
-    );
+    return (<div>
+       <Star />
+       <Star />
+       <Star />
+       <Star />
+       <Star />
+    </div>);
 }
-function Accordion(){
-    return <div>
-        <h3>Меню</h3>
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
-    </div>
+function Star(){
+    return(<div>Star</div>)
 }
-
-
+    function Accordion(){
+        return(<div>
+            <h3>Меню</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>)
+    }
 
 export default App;
