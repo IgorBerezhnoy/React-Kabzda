b7d5c51bf9e7e88931c41f5c5ae81676278d3ae5
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-07-27 16:19:04 +0800

    finished lesson 20 about useState

---
+++ src/stories/UseState.stories.tsx
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import {log} from 'util';
+
+export default {
+    title: 'useState',
+
+};
+
+
+export const Example1 = () => {
+
+    const generateData = () => {
+        console.log("generate");
+      return 32849872735
+    }
+
+    const [counter, setCounter] = useState(generateData);
+    const onClickHandler = () => {
+
+        setCounter(changer);
+    };
+    const changer=(state:number)=>{
+        console.log(state);
+        return state+1
+    }
+
+    return <>
+        {counter}
+        <button onClick={onClickHandler}>+</button>
+
+    </>;
+};
