89f38e11a7e09f5920fb67de1a68c384affa05d7
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-08-04 17:04:19 +0800

    finished lesson 23 added anologClock

---
+++ src/components/Clock/AnalogClock.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {Clock} from './Clock';
+import {AnalogClock} from './AnologClock';
+
+
+export default {
+    title: 'AnalogClock',
+    component: AnalogClock
+};
+export const BaseExample = () => {
+    return <AnalogClock/>;
+};
---
+++ src/components/Clock/AnologClock.tsx
@@ -0,0 +1,65 @@
+import React, {useEffect, useState} from 'react';
+import './AnologClock.css';
+import useEffectStories from '../../stories/UseEffect.stories';
+
+
+export const AnalogClock = () => {
+    let [date, setDate] = useState(new Date());
+    let seconds = date.getSeconds();
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+
+    useEffect(() => {
+        setInterval(() => {
+            setDate(new Date());
+            console.log("ttt");
+        },1000);
+    }, []);
+
+    return (
+
+        <div className="clock">
+            <div className="hand hour" style={{transform: `translate(-50%) rotate(${hours/12*360 }deg)`}}></div>
+            <div className="hand minute"style={{transform: `translate(-50%) rotate(${minutes/60*360}deg)`}}  ></div>
+            <div className="hand second"style={{transform: `translate(-50%) rotate(${seconds/60*360}deg)`}}  ></div>
+
+            <div className="number number1">
+                <div>1</div>
+            </div>
+            <div className="number number2">
+                <div>2</div>
+            </div>
+            <div className="number number3">
+                <div>3</div>
+            </div>
+            <div className="number number4">
+                <div>4</div>
+            </div>
+            <div className="number number5">
+                <div>5</div>
+            </div>
+            <div className="number number6">
+                <div>6</div>
+            </div>
+            <div className="number number7">
+                <div>7</div>
+            </div>
+            <div className="number number8">
+                <div>8</div>
+            </div>
+            <div className="number number9">
+                <div>9</div>
+            </div>
+            <div className="number number10">
+                <div>10</div>
+            </div>
+            <div className="number number11">
+                <div>11</div>
+            </div>
+            <div className="number number12">
+                <div>12</div>
+            </div>
+        </div>
+    );
+};
+
